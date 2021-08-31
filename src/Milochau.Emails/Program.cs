ac66d41e2c3c21f48020b4e3e706cf4a98c36020
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-08-31 14:40:10 +0200

    Try

--- src/Milochau.Emails/Program.cs
+++ src/Milochau.Emails/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Milochau.Core.Functions;
 using Milochau.Core.Functions.Infrastructure.Hosting;
+using Milochau.Core.Infrastructure.Hosting;
 
 namespace Milochau.Emails
 {
@@ -12,6 +16,29 @@ namespace Milochau.Emails
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureCoreHostBuilder<Startup>();
+                .ConfigureHostConfiguration(configurationBuilder =>
+                {
+                    var environmentName = CoreOptionsFactory.GetCurrentEnvironmentFromEnvironmentVariables();
+                    var hostName = CoreOptionsFactory.GetCurrentHostFromEnvironmentVariables();
+
+                    configurationBuilder
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: false)
+                        .AddJsonFile($"appsettings.{hostName}.json", optional: true, reloadOnChange: false);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var startup = CoreFunctionsStartup.Create<Startup>(hostContext.Configuration);
+                    startup.ConfigureServices(services);
+                    services.AddSingleton<CoreFunctionsStartup>(startup);
+                })
+                .ConfigureFunctionsWorkerDefaults((hostBuilderContext, functionsWorkerApplicationBuilder) =>
+                {
+                    var serviceProvider = functionsWorkerApplicationBuilder.Services.BuildServiceProvider();
+
+                    var startup = serviceProvider.GetRequiredService<CoreFunctionsStartup>();
+
+                    startup.Configure(serviceProvider, functionsWorkerApplicationBuilder);
+                });
     }
 }
