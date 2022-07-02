41d0071149ea54ca762f1c7a37dfbc45336960c1
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-07-02 18:40:56 +0200

    Remove unused logs

--- src/Milochau.Emails/Functions/EmailsFunctions.cs
+++ src/Milochau.Emails/Functions/EmailsFunctions.cs
@@ -1,6 +1,5 @@
 ﻿using Milochau.Emails.Services;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using System;
 using Milochau.Emails.Sdk.Models;
 using System.Linq;
@@ -29,20 +28,14 @@ namespace Milochau.Emails.Functions
         [Function("SendEmailFromServiceBus")]
         public async Task SendEmailFromServiceBusAsync([ServiceBusTrigger("emails")] Email email, FunctionContext context)
         {
-            var logger = context.GetLogger<EmailsFunctions>();
-            logger.LogDebug("Start running SendEmailFromServiceBus Function...");
-
             var errors = emailsValidationHelper.ValidateEmail(email);
             if (errors != null && errors.Any())
             {
                 var aggregatedErrors = errors.Aggregate((a, b) => a + Environment.NewLine + b);
-                logger.LogWarning("Email has not been sent, due do validation problems." + Environment.NewLine + aggregatedErrors);
                 throw new ArgumentException(nameof(Email), aggregatedErrors);
             }
 
             await emailsService.SendEmailAsync(email, CancellationToken.None);
-
-            logger.LogDebug("Email has been sent.");
         }
     }
 }
